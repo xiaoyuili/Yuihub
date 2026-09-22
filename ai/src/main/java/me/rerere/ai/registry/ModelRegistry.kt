3f3aba5bdc636648b65cc1e7ceea986d0164f2e6
re-ovo <1609403959@qq.com>
2026-09-22 10:24:15 +0800

    chore: mimo v2.6注册

--- ai/src/main/java/me/rerere/ai/registry/ModelRegistry.kt
+++ ai/src/main/java/me/rerere/ai/registry/ModelRegistry.kt
@@ -537,6 +537,12 @@ object ModelRegistry {
         toolReasoningAbility()
     }
 
+    private val XIAOMI_MIMO_V2_6 = defineModel {
+        tokens("mimo", "v", "2", "6")
+        visionInput()
+        toolReasoningAbility()
+    }
+
     private val XIAOMI_MIMO_V3 = defineModel {
         tokens("mimo", "v", "3")
         visionInput()
@@ -666,6 +672,7 @@ object ModelRegistry {
         XIAOMI_MIMO_V2_PRO,
         XIAOMI_MIMO_V2_5,
         XIAOMI_MIMO_V2_5_PRO,
+        XIAOMI_MIMO_V2_6,
         XIAOMI_MIMO_V3,
         XIAOMI_MIMO_V3_PRO,
         HY3,
